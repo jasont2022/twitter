fdd9ece6fe8d42acf975a07d0924162c255972bf
jasont2022 <jasontran2022@gmail.com>
2020-05-02 00:50:11 +0000

    created user views and controllers, now working on setting the nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :tweets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  devise_for :users
+  devise_for :users, :path => 'accounts'
+  resources :user do
+    resources :tweets
+  end
 end
