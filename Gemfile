8a69510eb0e691399c93681f70e0d92bcccfca8c
jasont2022 <jasontran2022@gmail.com>
2020-05-09 02:19:49 +0000

    finsihed project

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.4'
 
 gem 'aws-sdk-s3'
+gem 'pry'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,18 @@ before_action :authenticate_user!
 
   # home page
   def home
-    @tweet = current_user.tweets.build
     @feed = Array.new
-      current_user.followings.each do |following| 
-        following.tweets.each do |tweet|
-          @feed << tweet
-        end
-      end
-      current_user.tweets.each do |tweet|
+    current_user.followings.each do |following| 
+      following.tweets.each do |tweet|
         @feed << tweet
       end
+    end
+    current_user.tweets.each do |tweet|
+      @feed << tweet
+    end
+    @feed.compact
+    @tweet = current_user.tweets.build
+    @feed.sort_by! { |tweet| tweet.created_at }
+    @feed.reverse!
   end
-  @feed.compact
 end
